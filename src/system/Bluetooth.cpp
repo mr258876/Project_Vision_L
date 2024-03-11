aa5dae924b1c3477edd0686fbc56f1db39141490
mr258876 <2231327533@qq.com>
2024-03-11 23:06:00 +0800

    gatt cts service test

--- src/system/Bluetooth.cpp
+++ src/system/Bluetooth.cpp
@@ -67,7 +67,7 @@ class AdvertisedDeviceCallbacks : public NimBLEAdvertisedDeviceCallbacks
     uint64_t addr;
     uint8_t device_type;
     memcpy(&addr, device->getAddress().getNative(), 6);
-    ble_rpa_replace_peer_params_with_rl((uint8_t*)&addr, &device_type, &pre);
+    ble_rpa_replace_peer_params_with_rl((uint8_t *)&addr, &device_type, &pre);
 
     ESP_LOGI(TAG, "Found device: %s", device->getAddress().toString().c_str());
     ESP_LOGI(TAG, "Address: %s", NimBLEAddress(addr, device_type).toString().c_str());
@@ -76,6 +76,50 @@ class AdvertisedDeviceCallbacks : public NimBLEAdvertisedDeviceCallbacks
     ESP_LOGI(TAG, "Is bonded: %d", NimBLEDevice::isBonded(device->getAddress()));
     ESP_LOGI(TAG, "Device has CTS: %d\n", device->isAdvertisingService(CURRENT_TIME_SERVICE_UUID));
     // scanResultHandler(device->getAddress().getNative(), device->getRSSI());
+  }
+};
+
+// Callback class for handling CTS characteristic read and write events
+class CurrentTimeCharacteristicCallbacks : public NimBLECharacteristicCallbacks
+{
+  void onRead(NimBLECharacteristic *pCharacteristic)
+  {
+    // Update the characteristic value with the current time
+    time_t now = time(nullptr);
+    struct tm *currentTime = localtime(&now);
+    uint8_t timeData[10] = {0};
+    timeData[0] = (currentTime->tm_year + 1900) % 256;
+    timeData[1] = (currentTime->tm_year + 1900) / 256;
+    timeData[2] = currentTime->tm_mon + 1;
+    timeData[3] = currentTime->tm_mday;
+    timeData[4] = currentTime->tm_hour;
+    timeData[5] = currentTime->tm_min;
+    timeData[6] = currentTime->tm_sec;
+    timeData[7] = 0; // Day of week (not used)
+    timeData[8] = 0; // Fractions256 (not used)
+    timeData[9] = 0; // Adjust reason (not used)
+    pCharacteristic->setValue(timeData, 10);
+  }
+
+  void onWrite(NimBLECharacteristic *pCharacteristic)
+  {
+    // Handle time update from a connected client
+    std::string value = pCharacteristic->getValue();
+    if (value.length() == 10)
+    {
+      // Extract time components from the written value
+      struct tm newTime;
+      newTime.tm_year = value[0] + value[1] * 256 - 1900;
+      newTime.tm_mon = value[2] - 1;
+      newTime.tm_mday = value[3];
+      newTime.tm_hour = value[4];
+      newTime.tm_min = value[5];
+      newTime.tm_sec = value[6];
+      // Set the system time to the updated value
+      time_t newEpochTime = mktime(&newTime);
+      struct timeval newTimeVal = {newEpochTime, 0};
+      settimeofday(&newTimeVal, nullptr);
+    }
   }
 };
 
@@ -103,7 +147,19 @@ void bluetooth_init()
 
   // Create a server
   pBLEServer = NimBLEDevice::createServer();
+
+  // Create current time service
+  NimBLEService *pCurrentTimeService = pBLEServer->createService(CURRENT_TIME_SERVICE_UUID);
+  NimBLECharacteristic *pCurrentTimeCharacteristic = pCurrentTimeService->createCharacteristic(
+      CURRENT_TIME_CHARACTERISTIC_UUID,
+      NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY | NIMBLE_PROPERTY::WRITE_ENC);
+
+  pCurrentTimeCharacteristic->setCallbacks(new CurrentTimeCharacteristicCallbacks());
+  pCurrentTimeService->start();
+
+  // Start advertising
   pBLEAdvertising = NimBLEDevice::getAdvertising();
+  pBLEAdvertising->addServiceUUID(pCurrentTimeService->getUUID());
   pBLEAdvertising->start();
 
   // Create a client
